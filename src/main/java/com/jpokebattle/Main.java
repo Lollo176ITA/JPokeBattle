5e9db51f60d98c4bdd53c8f87f90f6f7f301b8a5
l.censi <censilorenzo1@gmail.com>
2024-05-10 14:42:01 +0200

    creazione beta brutta SchermataIniziale.fxml e swich pagine

--- src/main/java/com/jpokebattle/Main.java
+++ src/main/java/com/jpokebattle/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
 
-            Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("controllersAndScenes/SchermataIniziale.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
---
+++ src/main/java/com/jpokebattle/controllersAndScenes/BattagliaController.java
@@ -0,0 +1,4 @@
+package main.java.com.jpokebattle.controllersAndScenes;
+
+public class BattagliaController {
+}
